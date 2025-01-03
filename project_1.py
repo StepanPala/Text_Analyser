6e1f8a4ccc09e10f7f5d97388d75504a329655d4
StepanPala <Palast@seznam.cz>
2025-01-03 10:33:42 +0100

    v1.1 for review

--- project_1.py
+++ project_1.py
@@ -46,90 +46,84 @@ users = {
 # Login verification
 username = input("Please enter your username: ")
 if users.get(username) != input("Please enter your password: "):
-    
+
     # User not recognized
     print("Unregistered user, terminating program…")
-    exit()
 
 # Login successful
 else:
     print(
-        f"{"-" * 42}\n"
+        f"{'-' * 42}\n"
         f"Welcome, {username.title()}.\n"
         f"There are three texts to be analysed.\n"
-        f"{"-" * 42}"
+        f"{'-' * 42}"
         )
-  
+
     # Text analysis
     allowed_input = input("Please enter 1, 2 or 3 to select a text: ")
-    print(f"{"-" * 42}")
+    print(f"{'-' * 42}")
 
     # Input check
     # Not a number
     if not allowed_input.isdigit():
         print("The entered value is not a number, terminating program…")
-        exit()
 
     # Wrong number
-    if allowed_input not in ["1", "2", "3"]:
-         print("You have entered an invalid number, terminating program…")
-         exit()
+    elif allowed_input not in ["1", "2", "3"]:
+        print("You have entered an invalid number, terminating program…")
 
     else:
         # Text split
         index = int(allowed_input) - 1
         words = texts[index].split()
-        
+
         # Number of words
         words_number = len(words)
-        
+
         # Titlecase words
-        words_title = [titlecase for titlecase in words if titlecase.istitle()]
-        words_title_num = len(words_title)
-        
+        words_title = len([titlecase for titlecase in words if titlecase.istitle()])
+
         # Uppercase words
-        words_upper = [uppercase for uppercase in words if uppercase.isupper() and uppercase.isalpha()]
-        words_upper_num = len(words_upper)
-        
+        words_upper = len(
+            [uppercase for uppercase in words if uppercase.isupper() and uppercase.isalpha()]
+            )
+
         # Lowercase words
-        words_lower = [lowercase for lowercase in words if lowercase.islower()]
-        words_lower_num = len(words_lower)
-        
+        words_lower = len([lowercase for lowercase in words if lowercase.islower()])
+
         # Numeric strings and their sum
-        numeric_strings = [numeric for numeric in words if numeric.isdigit()]
-        numeric_strings_num = len(numeric_strings)
-        num_sum = sum(int(numeric) for numeric in numeric_strings)
-        
+        numeric_strings = len([numeric for numeric in words if numeric.isdigit()])
+        num_sum = sum(int(numeric) for numeric in words if numeric.isdigit())
+
         # Print the results
         print(
             f"There are {words_number} words in the text.\n"
-            f"There are {words_title_num} titlecase words in the text.\n"
-            f"There are {words_upper_num} uppercase words in the text.\n"
-            f"There are {words_lower_num} lowercase words in the text.\n"
-            f"There are {numeric_strings_num} numeric strings in the text.\n"
+            f"There are {words_title} titlecase words in the text.\n"
+            f"There are {words_upper} uppercase words in the text.\n"
+            f"There are {words_lower} lowercase words in the text.\n"
+            f"There are {numeric_strings} numeric strings in the text.\n"
             f"The sum of all the numbers is {num_sum}."
             )
-        
+
         # Graphical representation
         # Words length and number
+        import re
         words_len = {}
         for word in words:
-            word_clean = word.strip(",.?!")
-            if word_clean:
-                length = len(word_clean)
-                if length in words_len:
-                    words_len[length] += 1
-                else:
-                    words_len[length] = 1
-        words_len_sorted = dict(sorted(words_len.items()))
-                
+            # Removes unwanted characters
+            word_clean = re.sub(r"[^a-zA-Z0-9]", "", word.strip())
+            length = len(word_clean)
+            words_len.setdefault(length, 0)
+            words_len[length] += 1
+            words_len_sorted = dict(sorted(words_len.items()))
+
         # Print the chart
         print(
-            f"{"-" * 42}\n"
-            f"LEN|{"OCCURENCES":^20}|NR.\n"
-            f"{"-" * 42}\n"
+            f"{'-' * 42}\n"
+            f"LEN|{'OCCURENCES':^20}|NR.\n"
+            f"{'-' * 42}\n"
         )
         for idx, occurence in words_len_sorted.items():
             print(
-                f"{idx:>3}|{("*" * int(occurence)):<20}|{occurence:<3}"
+                f"{idx:>3}|{('*' * int(occurence)):<20}|{occurence:<3}"
             )
